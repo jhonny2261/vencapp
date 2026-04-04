ab8eefe2b944845b881466b6cfba5f0a962e5fd7
jhonny2261 <jhonnylorenzo@gmail.com>
2026-04-04 00:19:41 -0500

    Eliminando archivos del repositorio pero manteniéndolos localmente

--- dist/Detener.bat
+++
@@ -1,4 +0,0 @@
-echo @echo off > dist\Detener.bat
-echo taskkill /F /IM Controldevencimientos.exe ^>nul 2^>^&1 >> dist\DETENER.bat
-echo echo Servidor Detenido. >> dist\Deneter.bat
-echo pause >> dist\Detener.bat
